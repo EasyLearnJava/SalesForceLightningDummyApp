dff808d0aaf0ae324885d20e0a374015970bf6e9
Easy Learn Java <easylearnjava4u@gmail.com>
2018-01-24 10:50:11 -0600

    Update DummyContactsController.js

--- DummyContactsController.js
+++ DummyContactsController.js
@@ -1,5 +1,8 @@
 ({
     doInit : function(component, event, helper) {
         helper.findAllContactsHelper(component);
+    },
+    searchNameKeyChange: function(component, event, helper) {
+        helper.SearchAllContactsByNameKeyHelper(component, event);
     }
 })
