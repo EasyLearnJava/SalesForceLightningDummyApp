575862767399c6f758d6a3fe7e18f3017e2b4dc8
Easy Learn Java <easylearnjava4u@gmail.com>
2018-01-24 16:08:53 -0600

    Create DummyContactsPaginatorController.js

---
+++ DummyContactsPaginatorController.js
@@ -0,0 +1,10 @@
+({
+	previousPage : function(component) {
+        var pageChangeEvent = component.getEvent("pagePrevious");
+        pageChangeEvent.fire();
+	},    
+	nextPage : function(component) {
+        var pageChangeEvent = component.getEvent("pageNext");
+        pageChangeEvent.fire();
+	}
+})
