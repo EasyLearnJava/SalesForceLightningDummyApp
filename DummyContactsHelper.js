a92d14fa06791df8a6fc93231dd5de7c31455100
Easy Learn Java <easylearnjava4u@gmail.com>
2018-01-26 07:04:39 -0600

    Update DummyContactsHelper.js

--- DummyContactsHelper.js
+++ DummyContactsHelper.js
@@ -1,5 +1,6 @@
 ({
     findAllContactsHelper : function(component, page) {
+        alert('page : ' + page);
         var action = component.get("c.findAllContacts");
         action.setStorable();
         var pageSize = component.get("v.pageSize");
@@ -27,28 +28,6 @@
             }
         });
         var startTime = performance.now();
-        $A.enqueueAction(action);
-    },
-    
-    SearchAllContactsByNameKeyHelper : function(component, event) {
-        var searchKey = event.getParam("searchKey");
-        var action = component.get("c.searchByName");
-        action.setParams({
-            "searchKey": searchKey
-        });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                console.log(response.getReturnValue());
-                component.set("v.contactsList", response.getReturnValue());
-            } else if (state === "ERROR"){
-                var message = response.getError();
-                console.error("SearchAllContactsByNameKeyHelper : Error");
-                console.error(message);                
-            }else{
-                console.log("SearchAllContactsByNameKeyHelper : Unhandled return response Status")
-            }
-        });
         $A.enqueueAction(action);
     }
 })
