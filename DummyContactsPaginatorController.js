f1989ea54355fe15e322ae93a19adfdb6bcefebe
Easy Learn Java <easylearnjava4u@gmail.com>
2018-01-26 07:06:35 -0600

    Update DummyContactsPaginatorController.js

--- DummyContactsPaginatorController.js
+++ DummyContactsPaginatorController.js
@@ -2,7 +2,8 @@
 	previousPage : function(component) {
         var pageChangeEvent = component.getEvent("pagePrevious");
         pageChangeEvent.fire();
-	},    
+	}, 
+    
 	nextPage : function(component) {
         var pageChangeEvent = component.getEvent("pageNext");
         pageChangeEvent.fire();
